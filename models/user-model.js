c40af913b1f100fb1105998e84071d995e45d35e
Mansi <mansishinde2101@gmail.com>
2024-07-25 16:37:40 +0530

    added seperation of concerns in models and created owner mmodel

---
+++ models/user-model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/chrosneaks");
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type:String,
+        trim:true,
+        minlegnth: 3,
+    },
+    email: String,
+    password: String,
+    cart: {
+        type: Array,
+        default: []
+    },
+    isadmin: Boolean,
+    orders: {
+        type: Array,
+        default: []
+    },
+    picture: String,
+    contact: Number,
+});
+
+module.exports = mongoose.model("user", userSchema);
